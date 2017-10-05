258f01659fb0a851a5279feecfbbba1cdde59662
Ricardo Miranda <ricardomirannda@gmail.com>
2017-10-05 20:13:20 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,7 +2,10 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+``var meuArray = [1, 'Frontend', true, 10, "Ricardo Miranda"];
+
+meuArray;
+
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
