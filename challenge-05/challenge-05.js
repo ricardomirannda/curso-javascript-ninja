eeeeb91bbe81f73edeb73b363f412f3b27b5518f
Ricardo Miranda <ricardomirannda@gmail.com>
2017-10-05 20:27:21 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,15 +2,18 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-``var meuArray = [1, 'Frontend', true, 10, "Ricardo Miranda"];
-
-meuArray;
+``var meuArray = [1, 'Frontend', true, 10, {Nome: "Ricardo"}];
+  meuArray;
 
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+``
+//Funcao literal
+function minhaFuncao(parametroArray){
+  return parametroArray;  
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
