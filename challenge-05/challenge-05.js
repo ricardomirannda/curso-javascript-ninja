fe967d1aac6627b477eab372af6ed34b346322ce
Ricardo Miranda <ricardomirannda@gmail.com>
2017-10-05 22:54:58 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -98,14 +98,15 @@ function book(bookName){
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+book();
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log("O livro Fortaleza Digital tem " + book('Fortaleza Digital').quantidadePaginas + " páginas");
+
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
