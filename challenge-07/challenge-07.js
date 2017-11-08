3f58fc71f74ce040818eb35f5dea463cbfe3826d
Ricardo Miranda <ricardomirannda@gmail.com>
2017-11-07 22:12:34 -0300

    Update challenge-07.js

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -17,13 +17,15 @@ function addItem (item){
   meuArray.push(item);
   return meuArray;
 }
+addItem("15")
 
 
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-// ?
+addItem([3, true, 'Miranda']);
+console.log(addItem())
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
