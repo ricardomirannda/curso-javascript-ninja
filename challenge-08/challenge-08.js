4c3d68914e9d548f43f9a3d9d038c8b28239a452
Ricardo Miranda <ricardomirannda@gmail.com>
2018-01-18 11:03:34 -0300

    teste

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -22,7 +22,7 @@ Mostre no console o nome da função criada acima, com a frase:
 Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
-// ?
+// ? 
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
